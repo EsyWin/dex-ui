5c4e8ee3d07cec13df8456b64de9c1f6688dcbb4
EsyWin <dev@krk.finance>
2022-07-23 13:53:26 +0200

    initial commit

---
+++ src/components/HelpUrls.ts
@@ -0,0 +1,12 @@
+export const helpUrls = {
+  customerSupport: 'https://t.me/ProjectSerum',
+  customerSupportZh: 'https://t.me/ProjectSerum_Chinese',
+  contactEmail: 'mailto:contact@projectserum.com',
+  discord: 'https://discord.gg/raydium',
+  telegram: 'https://t.me/raydiumprotocol',
+  github: 'https://github.com/raydium-io/raydium-dex',
+  twitter: 'https://twitter.com/RaydiumProtocol',
+  raydium: 'https://raydium.io/',
+  developerResources: 'https://projectserum.com/developer-resources',
+  solanaBeach: 'https://solanabeach.io',
+};
